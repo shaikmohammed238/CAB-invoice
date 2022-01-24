667b64f7e4f5eec61ed39066ddae5dc9d37cce91
shaikmohammed238 <shaikmohammedghoue238@gmail.com>
2022-01-24 08:55:53 +0530

    added uc4

---
+++ day30_Cab_Invoice_Generator/RideRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace day30_Cab_Invoice_Generator
+{ /* UC4:- Invoice Service.
+             - Given a User Id, the invoice Service gets the list of rides from the RideRepository, and return the Invoice.
+         */
+    public class RideRepository // Ride Repository Class To Store Cab Rides And Get Cab Rides By User Id.
+    {
+        private static Dictionary<string, List<Rides>> userRideList = new Dictionary<string, List<Rides>>(); // Create Dictionary
+
+        public static void AddRides(string userID, Rides[] ride) // Static Method For Adding Ride Details By User Id.
+        {
+            bool checkList = userRideList.ContainsKey(userID);
+
+            if (checkList == false)
+            {
+                userRideList.Add(userID, new List<Rides>(ride)); //add ride
+            }
+        }
+        public static Rides[] GetRides(string userId) // Static Method To Get Cab Rides Of User By User Id.
+        {
+            return userRideList[userId].ToArray(); //Cab Rides Of User
+        }
+    }
+}
