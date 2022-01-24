ecf7bde4043e83390e709fd022b8717495a4eed0
shaikmohammed238 <shaikmohammedghoue238@gmail.com>
2022-01-24 07:19:12 +0530

    added master

---
+++ day30_Cab_Invoice_Generator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace day30_Cab_Invoice_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME CAB INVOIC GENERATOR");
+        }
+    }
+}
